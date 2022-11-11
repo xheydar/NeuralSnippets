5cd3b01be4322ed79e7888e04a29b278862a9f91
Heydar Maboudi Afkham <heydar@gmail.com>
2022-11-11 17:38:31 +0100

    Minor Updates

--- GAN/train.py
+++ GAN/train.py
@@ -15,10 +15,10 @@ import model
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0)
 
 class module :
     def __init__( self, dset ):
