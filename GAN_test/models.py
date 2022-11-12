98cb5a384e8fd06bd1b43f075033372b5f95fb34
Heydar Maboudi Afkham <heydar@gmail.com>
2022-11-12 23:04:59 +0100

    Minor Updates

--- GAN_test/models.py
+++ GAN_test/models.py
@@ -7,7 +7,6 @@ class Encoder(nn.Module):
 
         self.main = nn.Sequential(
             nn.Conv2d(nc, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d( ngf ),
             nn.ReLU(inplace=True),
             nn.Conv2d(ngf, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
@@ -92,7 +91,6 @@ class Discriminator(nn.Module):
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
