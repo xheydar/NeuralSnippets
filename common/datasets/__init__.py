48efc5c1c3d03f07e01bb444be0a8309e9d3abc1
Heydar Maboudi Afkham <heydar@gmail.com>
2022-11-13 08:19:20 +0100

    Minor Updates

--- common/datasets/__init__.py
+++ common/datasets/__init__.py
@@ -1,8 +1,8 @@
 from .mnist import mnist_dataset
-from .fashion_mnist import fashion_mnist
+from .fashion_mnist import fashion_mnist_dataset
 from .cifar10 import cifar10_dataset
 
 datasets = {}
 datasets['mnist'] = mnist_dataset
-datasets['fashion_mnist'] = fashion_mnist
+datasets['fashion_mnist'] = fashion_mnist_dataset
 datasets['cifar10'] = cifar10_dataset
