1a57528f6a50bb3cd58d254c2e7bac3b127bf61e
Heydar Maboudi Afkham <heydar@gmail.com>
2022-11-13 08:20:04 +0100

    Minor Updates

---
+++ common/datasets/fashion_mnist.py
@@ -0,0 +1,19 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+class fashion_mnist_dataset :
+    def __init__( self, root, train ):
+        transform=transforms.Compose([
+                           transforms.Resize(28),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.5,), (0.5,)),
+                       ])
+
+        self.dataset = torchvision.datasets.FashionMNIST( root='../data', 
+                                                          train=train, 
+                                                          transform=transform, 
+                                                          download=True )
+
+        self.dim = 784
+        self.shape = [1,28,28]
