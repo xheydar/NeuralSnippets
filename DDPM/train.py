db482a87036b9495f412ba3c9d178c1c9b13964b
Heydar Maboudi Afkham <heydar@gmail.com>
2022-11-20 23:04:44 +0100

    Minor Updates

--- DDPM/train.py
+++ DDPM/train.py
@@ -45,55 +45,15 @@ class module :
 
     def do_stuff( self ):
         x_noisy, noise, t, y = self.batches[10]
-
-        out = self.model['unet']( x_noisy, t )
-
-
-        return
-
-
-
-
-        x = self.batches[10].to(self.device)
-
-        t = torch.randint(0, self.timesteps, (self.batch_size,), device=self.device).long()
-
-        x_noisy, noise = self.noise_scheduler.forward_diffusion_sample( x, t )
-        noise_pred = self.model['unet'](x_noisy, t)
-        loss = self.model['loss']( noise, noise_pred )
+        
+        pred_noise = self.model['unet']( x_noisy, t )
+        loss = self.model['loss']( pred_noise, noise.detach() )
 
         print( loss )
 
-        print( loss.item() )
-
-        #t = torch.Tensor([50]).type(torch.int64)
-        #image, noise = self.noise_scheduler.forward_diffusion_sample( batch, t )
-
-        #image = self.dataset.reverse_transform( image[0] )
-        #pp.imshow(image)
-
-    @torch.no_grad()
-    def sample_plot_image( self ):
-        # Sample noise
-        img_size = self.image_size
-        img = torch.randn((1, 3, img_size, img_size), device=device)
-        #plt.figure(figsize=(15,15))
-        #plt.axis('off')
-        num_images = 10
-        stepsize = int(self.timesteps/num_images)
-
-        for i in range(0,self.timesteps)[::-1]:
-            t = torch.full((1,), i, device=device, dtype=torch.long)
-            noise_pred = self.model['unet']( img, t )
-            img = self.noise_scheduler.sample_timestep(img, t, noise_pred)
-            #if i % stepsize == 0:
-            #    plt.subplot(1, num_images, i/stepsize+1)
-            #    show_tensor_image(img.detach().cpu())
-        #plt.show()
-
     def train( self, num_epoch=100 ):
         optimizer = optim.Adam(self.model['unet'].parameters(), lr=0.001)
-        dataloader = torch.utils.data.DataLoader(dataset=self.dataset.dataset, batch_size=self.batch_size, shuffle=True)
+        dataloader = torch.utils.data.DataLoader( self.batches, batch_size=None, num_workers=8 )
 
         if not os.path.exists('./snapshots') : 
             os.makedirs('./snapshots')
@@ -101,17 +61,12 @@ class module :
         for epoch in range(num_epoch):
             print("Epoch : %d/%d" % ( epoch+1, num_epoch ) )
             loss_values = []
-            for batch_idx, batch in tqdm(enumerate(dataloader)):
+            for batch_idx, ( x_noisy, noise, t, y ) in tqdm(enumerate(dataloader)):
 
                 optimizer.zero_grad()
 
-                x = batch[0].to(self.device)
-                t = torch.randint(0, self.timesteps, (len(x),), device=self.device).long()
-                
-                x_noisy, noise = self.noise_scheduler.forward_diffusion_sample( x, t, device=self.device )
-
-                noise_pred = self.model['unet'](x_noisy, t)
-                loss = self.model['loss']( noise, noise_pred )
+                pred_noise = self.model['unet']( x_noisy, t )
+                loss = self.model['loss']( pred_noise, noise.detach() )
 
                 loss_values.append( loss.item() )
 
