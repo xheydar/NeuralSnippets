93af29c47b1d89b0f0df28dffbb971f13d56b4de
Heydar Maboudi Afkham <heydar@gmail.com>
2023-01-17 12:53:10 +0100

    Minor Updates

--- Detection/yolo_base/train.py
+++ Detection/yolo_base/train.py
@@ -255,7 +255,7 @@ class Train :
                     accumulate = max(1, np.interp(ni, xi, [1, nbs / total_batch_size]).round())
                     for j, x in enumerate(optimizer.param_groups):
                         # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
-                        x['lr'] = np.interp(ni, xi, [hyp['warmup_bias_lr'] if j == 2 else 0.0, x['initial_lr'] * lf(epoch)])
+                        x['lr'] = np.interp(ni, xi, [self.hyp['warmup_bias_lr'] if j == 2 else 0.0, x['initial_lr'] * lf(epoch)])
                         if 'momentum' in x:
                             x['momentum'] = np.interp(ni, xi, [self.hyp['warmup_momentum'], self.hyp['momentum']])
  
