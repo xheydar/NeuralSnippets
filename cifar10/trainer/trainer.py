863321365ec7f0db9a8ce6da92dc0173eb93d452
Heydar Maboudi Afkham <heydar@gmail.com>
2023-12-06 21:52:03 +0100

    Minor Updates

--- cifar10/trainer/trainer.py
+++ cifar10/trainer/trainer.py
@@ -11,6 +11,36 @@ class trainer :
     def __init__( self ):
         pass
 
+    def get_optimizer( self, name='SGD', lr=0.001, momentum=0.9, decay=1e-5 ): 
+        g = [], [], []  # optimizer parameter groups
+        bn = tuple(v for k, v in nn.__dict__.items() if 'Norm' in k)  # normalization layers, i.e. BatchNorm2d()
+        for v in self.model['net'].modules():
+            for p_name, p in v.named_parameters(recurse=0):
+                if p_name == 'bias':  # bias (no decay)
+                    g[2].append(p)
+                elif p_name == 'weight' and isinstance(v, bn):  # weight (no decay)
+                    g[1].append(p)
+                else:
+                    g[0].append(p)  # weight (with decay)
+
+
+        if name == 'Adam':
+            optimizer = torch.optim.Adam(g[2], lr=lr, betas=(momentum, 0.999))  # adjust beta1 to momentum
+        elif name == 'AdamW':
+            optimizer = torch.optim.AdamW(g[2], lr=lr, betas=(momentum, 0.999), weight_decay=0.0)
+        elif name == 'RMSProp':
+            optimizer = torch.optim.RMSprop(g[2], lr=lr, momentum=momentum)
+        elif name == 'SGD':
+            optimizer = torch.optim.SGD(g[2], lr=lr, momentum=momentum, nesterov=True)
+        else:
+            raise NotImplementedError(f'Optimizer {name} not implemented.')
+
+        optimizer.add_param_group({'params': g[0], 'weight_decay': decay})  # add g0 with weight_decay
+        optimizer.add_param_group({'params': g[1], 'weight_decay': 0.0})  # add g1 (BatchNorm2d weights)
+        #print(f"{colorstr('optimizer:')} {type(optimizer).__name__}(lr={lr}) with parameter groups "
+        #      f"{len(g[1])} weight(decay=0.0), {len(g[0])} weight(decay={decay}), {len(g[2])} bias")
+        return optimizer
+
     def train_step( self, train_loader, optimizer, ema=None ):
 
         self.model['net'].train()
@@ -76,7 +106,7 @@ class trainer :
         train_loader = self.datasets['train'].get_loader( 16, True )
         test_loader = self.datasets['test'].get_loader( 16, False )
 
-        optimizer = optim.SGD( self.model['net'].parameters(), lr=0.001, momentum=0.9)
+        optimizer = self.get_optimizer('SGD', lr=0.001, momentum=0.9, decay=1e-5 )
 
         train_loss = []
         test_acc = []
